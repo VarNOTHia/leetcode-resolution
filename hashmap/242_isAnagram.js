d192e74fc9bbfd5216e1ef50523a3308c10203b6
VarNOTHia <varnothia.outlook.com>
2024-05-29 18:33:07 +0800

    哈希表系列题解

---
+++ hashmap/242_isAnagram.js
@@ -0,0 +1,27 @@
+const isAnagram = (s, t) => {
+  const record = new Map();
+  if(s.length !== t.length) return false;
+  for(let i of s){
+    if(!record.has(i)){
+      record.set(i, 1);
+    }else{
+      record.set(i, record.get(i) + 1);
+    }
+  }
+
+  for(let i of t){
+    // 遍历字符串 s 的时候，对这个哈希表的初始化就决定每个字母的值不可能小于 0，所以不需要判断它小于 0 的情况。
+    if(!record.get(i)){
+      return false;
+    }else{
+      record.set(i, record.get(i) - 1);
+    }
+  }
+
+  return true;
+}
+
+// 解法二
+var isAnagram_ = function(s, t) {
+  return s.length == t.length && [...s].sort().join('') === [...t].sort().join('');
+};
